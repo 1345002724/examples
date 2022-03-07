7085c8b159b129bf1de2314a793d4ede9d4831cd
liu <1345002724@qq.com>
2022-03-07 10:24:08 +0800

    20211023提交

---
+++ src/main/java/com/jsg/examples/task/TokenTask2.java
@@ -0,0 +1,22 @@
+package com.jsg.examples.task;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+//@EnableScheduling
+public class TokenTask2 {
+
+
+    @Scheduled(fixedRate = 2000)
+    public void start(){
+        System.out.println("定时器每2秒执行一次");
+    }
+    
+}
+
